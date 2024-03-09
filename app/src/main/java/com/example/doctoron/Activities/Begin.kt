2a83cdac2538873eb61981656e4713b45bb5c16e
ToanLe <whiteboyhihihi@gmail.com>
2024-03-09 16:45:55 +0700

    tinh chỉnh begin

--- app/src/main/java/com/example/doctoron/Activities/Begin.kt
+++ app/src/main/java/com/example/doctoron/Activities/Begin.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.widget.AppCompatButton
 import com.example.doctoron.R
 
 class Begin : AppCompatActivity() {
+    private val k : String=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_begin)
